b7cf6f140f77ff83db28614b29820b8130190891
Casimir Chiong <76983619+casimir-chiong@users.noreply.github.com>
2023-07-24 17:27:05 +0800

    func main

--- solver.py
+++ solver.py
@@ -1,3 +1,37 @@
+import time
+import pandas as pd
 import numpy as np
 
-game = np.array([])
+
+def main():
+    # Start the game
+    game = start_game()
+
+    # Solve the game
+    start_time = time.time()
+    solution = solve_game(game)
+    end_time = time.time()
+
+    # Check the solution
+    if valid_solution(solution):
+        time_taken = end_time - start_time
+
+        print(game)
+        print(solution)
+        print(f"Time take to solve: {time_taken}s")
+
+
+def start_game():
+    ...
+
+
+def solve_game(game):
+    ...
+
+
+def valid_solution(solution):
+    ...
+
+
+if __name__ == "__main__":
+    main()
