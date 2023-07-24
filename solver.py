0cd2fb776655532d755e5f4fb08752d13895b5cb
Casimir Chiong <76983619+casimir-chiong@users.noreply.github.com>
2023-07-24 17:59:36 +0800

    func get_sudoku

--- solver.py
+++ solver.py
@@ -1,35 +1,49 @@
 import time
+import random
 import pandas as pd
 import numpy as np
 
 
 def main():
-    # Start the game
-    game = start_game()
+    # Get a sudoku
+    print("Getting the sudoku...")
+    sudoku, answer = get_sudoku()
+    print("Sudoku: ")
+    print(sudoku)
 
-    # Solve the game
+    # Solve the sudoku
     start_time = time.time()
-    solution = solve_game(game)
+    solution = solve_sudoku(sudoku)
     end_time = time.time()
 
     # Check the solution
-    if valid_solution(solution):
+    if valid_solution(answer, solution):
         time_taken = end_time - start_time
 
-        print(game)
         print(solution)
         print(f"Time take to solve: {time_taken}s")
 
 
-def start_game():
+def get_sudoku():
+    # Get a random sudoku and its answer
+    df = pd.read_csv("sudoku.csv")
+    choice = random.choice(df.values.tolist())
+    sudoku = [*choice[0]]
+    answer = [*choice[1]]
+
+    # Convert them into an numpy array
+    npsudoku = np.array(sudoku, np.int8).reshape(9, 9)
+    npanswer = np.array(answer, np.int8).reshape(9, 9)
+
+    # Return the sudoku and the answer
+    return npsudoku, npanswer
+
+
+def solve_sudoku(sudoku):
     ...
 
 
-def solve_game(game):
-    ...
-
-
-def valid_solution(solution):
+def valid_solution(answer, solution):
     ...
 
 
